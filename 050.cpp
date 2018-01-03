f39bf17156afbeb51af84e78d5309a7feb584114
Angda Song <songangda@gmail.com>
2018-01-02 23:41:30 -0800

    NEW: 050

---
+++ 050.cpp
@@ -0,0 +1,43 @@
+/*
+Implement pow(x, n).
+
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+
+ */
+
+
+// GentleSpoon me@gentlespoon.com
+// Leetcode    Q50
+// Start       2018-01-02-23-24-40
+// Finish      2018-01-02-23-39-35
+
+class Solution {
+public:
+    double fastPow(double x, long long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2) {
+            return half * half * x;
+        } else {
+            return half * half;
+        }
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+};
