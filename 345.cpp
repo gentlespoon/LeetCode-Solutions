011282ea531225143e0687c14db0bdca1ae3af11
Angda Song <songangda@gmail.com>
2018-01-03 01:53:29 -0800

    NEW: 345

---
+++ 345.cpp
@@ -0,0 +1,34 @@
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+
+Note:
+The vowels does not include the letter "y".
+ */
+
+
+// GentleSpoon me@gentlespoon.com
+// Leetcode    Q345
+// Start       2018-01-03-1-43-44
+// Finish      2018-01-03-1-51-58
+
+    
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+    }
+};
