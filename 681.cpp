44fe8c81d33687e3abde606b4ba434a1f16dd7e8
GentleSpoon <songangda@gmail.com>
2018-01-14 11:17:39 -0800

    Update 681.cpp

--- 681.cpp
+++ 681.cpp
@@ -21,23 +21,49 @@ Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22. It
 // Start       2017-11-14-14-27-38
 // Finish      2017-11-14-14-53-42
 
+class Solution {
+public:
+    int digits[4];
 
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main(int argc, char* argv[]) {
-    string time = "12:34";
-    vector<int> digit_array;
-    unordered_map<int, int> digit_umap;
-    for(int i = 0; i<time.length(); i++) {
-        if (isdigit(time[i])) {
-            digit_umap.insert({i, time[i]});
-            digit_array.push_back(time[i]);
+    /**
+     * Check if the digit is valid
+     * @param  val  The digit being checked
+     * @return      Boolean
+     */
+    bool isvalid(int val) {
+        for (int i = 0; i < 4; i++) {
+            if (val == digits[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    string nextClosestTime(string time) {
+        string timeWithoutColon = time;
+        timeWithoutColon.erase(2,1);
+        for(int i = 0; i<4; i++) {
+            digits[i] = timeWithoutColon[i]-'0';
+        }
+        int hour = stoi(time.substr(0,2));
+        int minute = stoi(time.substr(3,2));
+        for(int i = 0; i < 1440; i++) {            // worst case: exact same time in another day, 1440 min later
+            if (++minute == 60) {
+                minute = 0;
+                if (++hour == 24) {
+                    hour = 0;
+                }
+            }
+            if (isvalid(minute%10) &&
+                isvalid(minute/10) &&
+                isvalid(hour%10)   &&
+                isvalid(hour/10)
+                ) {
+                ostringstream oss;
+                oss << std::setw(2) << std::setfill('0') << hour << ":" << std::setw(2) << std::setfill('0') << minute;
+                return oss.str();
+            }
         }
     }
     
-    // cout << "ARGV: " << time << endl;
-    return 0;
-}
+};
