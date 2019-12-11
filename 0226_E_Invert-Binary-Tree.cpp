5549443418fd291d5cd65c981d8ff1aad3ed8d6c
Angda Song <github@gentlespoon.com>
2019-12-11 15:48:59 -0800

    Create 0226_E_Invert-Binary-Tree.cpp

---
+++ 0226_E_Invert-Binary-Tree.cpp
@@ -0,0 +1,55 @@
+// github.com/gentlespoon
+// 0226 - Easy - Invert Binary Tree
+// 2019-12-11-06-17-43
+// 2019-12-11-06-19-52
+// Runtime: 4 ms, faster than 57.22% of C++ online submissions for Invert Binary Tree.
+// Memory Usage: 9.2 MB, less than 87.27% of C++ online submissions for Invert Binary Tree.
+
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        TreeNode* right = invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
